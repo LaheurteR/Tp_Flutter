1414c077361b3e648618c8e02c84843aaadc66bc
LaheurteR <97089676+LaheurteR@users.noreply.github.com>
2024-05-21 15:10:08 +0200

    debut projet

---
+++ tp_1/lib/TP1_number.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  int price1 = 2;
+  double price2 = 3.0;
+  var sum = price1 + price2;
+  print(sum);
+  print(sum.runtimeType);
+  print (sum.toInt());
+
+  const String str7 = "7";
+  //const int num7 = int.parse(str7);
+
+  print("hello world");
+  String message = "Hello, World !";
+  const String hello = "Hello";
+  const String world = "world";
+  print("$hello, $world !");
+
+}
